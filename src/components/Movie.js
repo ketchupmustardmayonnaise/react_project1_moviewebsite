38aead78846b6860da89c96ece9d723f9d7c49dc
ketchupmustardmayonnaise <pinkmiro@khu.ac.kr>
2022-03-31 03:56:31 +0900

    css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css"
 
-function Movie({id, mediumCoverImage, title, summary, genres}){
+function Movie({id, mediumCoverImage, title, year, summary, genres}){
     return(
-        <div>
-        <img src={mediumCoverImage} />
-        <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+        <div className={styles.movie}>
+        <img src={mediumCoverImage} className={styles.movie__img}/>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link></h2>
+        <h3 className={styles.movie__year}>{year}</h3>
         <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-        <ul>
+        <ul className={styles.movie__genres}>
           {genres.map(g => <li key = {g}>{g}</li>)}
         </ul>
       </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from '../components/Movie';
+import styles from "./Home.module.css"
 
 function Home(){
     const [loading, setLoading] = useState(true);
@@ -16,16 +17,24 @@ function Home(){
     useEffect(() => {getMovies();}, []);
 
     return (
-        <div className="App">
-        {loading ? <h1>Loading...</h1> : movies.map((movie) => (
-            <Movie 
-            key={movie.id}
-            id={movie.id}
-            mediumCoverImage={movie.medium_cover_image}
-            title={movie.title}
-            summary={movie.summary}
-            genres={movie.genres}/>
-        ))}
+        <div className={styles.container}>
+        {loading ? (
+            <div className={styles.loader}>
+                <span>Loading...</span>
+            </div>
+        ) : (
+            <div className={styles.movies}>
+            {movies.map((movie) => (
+                <Movie 
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    mediumCoverImage={movie.medium_cover_image}
+                    title={movie.title}
+                    summary={movie.summary}
+                    genres={movie.genres} />))}
+            </div>
+        )}
         </div>
     );
 }
